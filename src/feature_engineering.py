9ee0bcbe7b6c55ad75abbbfcd4c2f809b6cbe235
ViniciusBG <viniciusbastosgomes@gmail.com>
2021-12-21 20:39:52 -0300

    fixing funcition

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -8,19 +8,20 @@ def generate_stockstats_features(ticker,train_predict="train"):
 
     conf = OmegaConf.load(f'conf/{ticker}.yaml')
 
-    features_to_generate = conf['stockstats_features']
+    features_to_generate = list(conf['stockstats_features'])
+    
 
     data = load_data(ticker=ticker,
                     train_predict=train_predict,
                     stage="raw")
-    TODO: "Debugg this part of the code"
-    print(data.columns)
+    
+
     stockstats_dataframe  = StockDataFrame.retype(data)
-
+    
     final_data = stockstats_dataframe[features_to_generate]
+    
+    return final_data
 
-
-    print(stockstats_dataframe)
 
 
 
